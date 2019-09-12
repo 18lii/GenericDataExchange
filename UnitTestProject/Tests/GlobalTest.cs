71c9ecaa5962898a24eb73f6b59bac3ca68bbbcd
Shine <ShinePhelesChou@hotmail.com>
2019-09-12 16:39:39 +0800

    配置文件修改

--- UnitTestProject/Tests/GlobalTest.cs
+++ UnitTestProject/Tests/GlobalTest.cs
@@ -58,16 +58,16 @@ namespace UnitTestProject.Tests
         public void WindowsServiceTest()
         {
             
-            //ServiceHost host = new ServiceHost(typeof(DataExchangeService));
+            ServiceHost host = new ServiceHost(typeof(DataExchangeService));
             var strs = new string[2]
             {
                 "FF50E1525C650A58AEAB16969808322F8A05F4F054F803F39A9EB1CA3EABBC36E55E402825C50E5BD28A79D5A17761F232CBE1657DA924823180E0634289856C59B54F3B24BEE5D2A58CEE37309BA49BD2509C331F7CC98E",
                 "30E9AA65BA938F545A6CC1DB3027CED9"
             };
             WCFServiceInitialization.Initialization(strs);
-            //host.Open();
+            host.Open();
             Console.ReadLine();
-            //host.Close();
+            host.Close();
         }
     }
 }
